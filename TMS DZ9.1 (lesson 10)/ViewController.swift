4272a70aafd61349184fadc6b54520d85b404a0a
Egor Kartshev <egorkarta982@gmail.com>
2022-06-28 17:45:33 +0300

    PanGastureRecognizer

--- TMS DZ9.1 (lesson 10)/ViewController.swift
+++ TMS DZ9.1 (lesson 10)/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  TMS DZ9.1 (lesson 10)
-//
-//  Created by Егор on 28.06.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var myCircle = UIView()
+    let side: CGFloat = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myCircle.frame = CGRect(x: self.view.frame.width / 2, y: self.view.frame.height / 2, width: side, height: side)
+        myCircle.backgroundColor = .blue
+        myCircle.layer.cornerRadius = myCircle.frame.width / 2
+        self.view.addSubview(myCircle)
+        
+        
+        let panGasture = UIPanGestureRecognizer(target: self, action: #selector(moveCircle))
+        self.myCircle.addGestureRecognizer(panGasture)
     }
-
-
+    
+    @IBAction func moveCircle(recognizer: UIPanGestureRecognizer){
+        let translation = recognizer.translation(in: self.view)
+        print(translation)
+        myCircle.center = CGPoint(x: myCircle.center.x + translation.x, y: myCircle.center.y + translation.y)
+        recognizer.setTranslation(CGPoint(x: 0, y: 0), in: self.view)
+        
+    }
+    
 }
+
 
